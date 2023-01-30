d832bc5d49938a889ee921d8528357d5cbe999d4
JVictor-CC <jvcunha1221@hotmail.com>
2023-01-29 21:45:01 -0300

    Finished design

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const UserInfo = ({profilePic, profileName, profileDesc, profileLogin}) => {
+  return (
+    <div className="user-info">
+        <img className="profile-pic" src={profilePic} alt="Profile picture"/>
+        <div>
+            <h3>{profileName}</h3>
+            <span>{profileLogin}</span>
+            <p>{profileDesc}</p>
+        </div>
+    </div>
+  )
+}
+
+export default UserInfo
